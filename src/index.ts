5305abd12ac9fde77f858f4c8d32b80ea0a3860e
Felipe Lourenco <felipe.lourenco@ingenico.com>
2021-10-05 16:05:53 +0200

    Adding build script with rollup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export const test = () => {
-  const a = 1
-  console.log('===================')
-  console.log(a)
-  console.log('===================')
-}
+import Button from './components/Button'
+
+export { Button }
